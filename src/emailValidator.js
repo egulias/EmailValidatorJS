0d3c56492bdc7880c5b7b0af31e41198e9c15b50
Eduardo Gulias Davis <me@egulias.com>
2015-01-06 01:31:33 +0100

    Initial Commit

---
+++ src/emailValidator.js
@@ -0,0 +1,5 @@
+module.exports = {
+  isValid: function(email) {
+    return false;
+  }
+}
---
+++ test/src/emailValidator.js
@@ -0,0 +1,8 @@
+var should = require('chai').should(),
+    emailValidator = require('../../src/emailValidator');
+
+describe('Is a valid email', function() {
+  it('Validates an email againts several RFCs', function(){
+    emailValidator.isValid('test').should.equal(true);
+  });
+});
