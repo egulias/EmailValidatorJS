4c93436520d323cb677fbfcc606e81baf543ccf0
Julien Castelain <jcastelain@gmail.com>
2015-01-27 22:25:16 +0100

    Parser: use prototype methods

--- src/parser.js
+++ src/parser.js
@@ -1,94 +1,102 @@
 'use strict';
 
-var lexer = require('./lexer');
+var lexer = require('./lexer').lexer;
 
 var NoLocalPart = {
-  name: "NoLocalPart",
-  message: "No local part found"
+  name: 'NoLocalPart',
+  message: 'No local part found'
 };
 
 var StartWithDot = {
-  name: "StartWithDot",
-  message: "Starts with a Dot"
+  name: 'StartWithDot',
+  message: 'Starts with a Dot'
 };
 
 var EndWithDot = {
-  name: "EndWithDot",
-  message: "Ends with a Dot"
+  name: 'EndWithDot',
+  message: 'Ends with a Dot'
 };
 
 var ExpectingATEXT = {
-  name: "ExpectingATEXT",
-  message: "ATEXT was expected"
+  name: 'ExpectingATEXT',
+  message: 'ATEXT was expected'
 };
 
-function LocalPartParser(lexer) {
-  Parser.call(this, lexer);
-  var parseDQUOTE = function (closingQuote) {
-    return false;
-  };
 
-  var isInvalidToken = function (token, closingQuote) {
-    var forbidden = [
-      {type:lexer.comma},
-      {type:lexer.closebracket},
-      {type:lexer.openbracket},
-      {type:lexer.greaterthan},
-      {type:lexer.lowerthan},
-      {type:lexer.colon},
-      {type:lexer.semicolon}
-      //lexer.invalid
-    ];
+function parseDQUOTE(closingQuote) {
+  return false; // ?
+};
 
-    forbidden.forEach(function(value) {
-      if (token.equals(value) && !closingQuote) {
-        throw ExpectingATEXT;
-      }
-    });
-  };
+function isInvalidToken(token, closingQuote) {
+  var forbidden = [
+    {type:lexer.comma},
+    {type:lexer.closebracket},
+    {type:lexer.openbracket},
+    {type:lexer.greaterthan},
+    {type:lexer.lowerthan},
+    {type:lexer.colon},
+    {type:lexer.semicolon}
+    //lexer.invalid
+  ];
 
-  this.parse = function () {
-    var parseDQuote = true;
-    var closingQuote = false;
-
-    while (lexer.token.type !== lexer.at && lexer.token) {
-
-      if (lexer.token.type === lexer.dot && !lexer.getPrevious()) {
-        throw StartWithDot;
-      }
-
-      closingQuote = this.checkDQUOTE(closingQuote);
-      if (closingQuote && parseDQuote) {
-        parseDQuote = parseDQUOTE();
-      }
-
-      if (lexer.token.type === lexer.openparenthesis) {
-        this.parseComments();
-      }
-
-      this.checkConsecutiveDots();
-
-      if (lexer.token.type === lexer.dot && lexer.isNextToken(lexer.at)) {
-        throw EndWithDot;
-      }
-
-      this.warnEscaping();
-      isInvalidToken(lexer.token, closingQuote);
-
-      if (this.isFWS()) {
-        this.parseFWS();
-      }
-
-      lexer.moveNext();
+  // Native forEach is really slow
+  // would you mind using lodash?
+  forbidden.forEach(function(value) {
+    if (token.equals(value) && !closingQuote) {
+      throw ExpectingATEXT;
     }
-  };
+  });
+};
+
+
+function LocalPartParser(lexer) {
+
+  Parser.call(this, lexer);
+
+
 
 }
 
 LocalPartParser.prototype = Object.create(Parser.prototype);
 LocalPartParser.prototype.constructor = LocalPartParser;
 
+LocalPartParser.prototype.parse = function () {
+  var parseDQuote = true;
+  var closingQuote = false;
+
+  while (lexer.token.type !== lexer.at && lexer.token) {
+
+    if (lexer.token.type === lexer.dot && !lexer.getPrevious()) {
+      throw StartWithDot;
+    }
+
+    closingQuote = this.checkDQUOTE(closingQuote);
+    if (closingQuote && parseDQuote) {
+      parseDQuote = parseDQUOTE();
+    }
+
+    if (lexer.token.type === lexer.openparenthesis) {
+      this.parseComments();
+    }
+
+    this.checkConsecutiveDots();
+
+    if (lexer.token.type === lexer.dot && lexer.isNextToken(lexer.at)) {
+      throw EndWithDot;
+    }
+
+    this.warnEscaping();
+    isInvalidToken(lexer.token, closingQuote);
+
+    if (this.isFWS()) {
+      this.parseFWS();
+    }
+    lexer.moveNext();
+  }
+};
+
 function Parser(lexer) {}
+
 Parser.prototype.checkDQUOTE = function (closingQuote) {
   return false;
 };
@@ -105,12 +113,13 @@ Parser.prototype.warnEscaping = function () {
 
 };
 
-Parser.prototype.isFWS = function () {
-  function escaped() {
-    return lexer.getPrevious().equals({type: lexer.backslash}) && !lexer.token.equals({type: lexer.generic});
-  }
+Parser.prototype.escaped = function (lexer) {
+  return lexer.getPrevious().equals({type: lexer.backslash}) && !lexer.token.equals({type: lexer.generic});
+};
 
-  if (escaped()) {
+Parser.prototype.isFWS = function () {
+
+  if (this.escaped(lexer)) {
     return false;
   }
 
@@ -125,20 +134,20 @@ Parser.prototype.parseFWS = function () {
 
 };
 
-function EmailParser () {
-  this.parse = function(email) {
-    lexer.lexer.lex(email);
-    hasLocalPart();
-    var localPartParser = new LocalPartParser(lexer.lexer);
-    localPartParser.parse();
-  };
+function hasLocalPart () {
+  if (lexer.token.type === lexer.at) {
+    throw NoLocalPart;
+  }
+};
 
-  var hasLocalPart = function() {
-    if (lexer.lexer.token.type === lexer.lexer.at) {
-      throw NoLocalPart;
-    }
-  };
+function EmailParser () { }
 
-}
+EmailParser.prototype.parse = function (email) {
+  lexer.lex(email);
+  hasLocalPart();
+  var localPartParser = new LocalPartParser(lexer);
+  localPartParser.parse();
+};
 
 module.exports.parser = new EmailParser();
+
